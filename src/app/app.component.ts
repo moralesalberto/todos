2f3e4c8f5ea729f7d4ccfc7b970ba364fb7737e0
moralesalberto <alberto@moralitos.com>
2022-08-05 23:33:35 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todos';
+	todos: Array<any> = [];
+	baseUrl: string = "http://localhost:3000/books";
+	
+	async getTodos() {
+		const resp = await fetch(this.baseUrl);
+		
+		const data = await resp.json();
+		this.todos = await data;
+	}
+	
+	ngOnInit() {
+		this.getTodos();
+	}
 }
